3307b2a5145254370b47953f67d2feed4d9436d4
gemamm <gemamm1988@gmail.com>
2021-10-15 11:08:41 +0200

    atc

--- src/com/ceep/dominio/Persona.java
+++ src/com/ceep/dominio/Persona.java
@@ -1,14 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ceep.dominio;
 
-/**
- *
- * @author Alumno Mañana
- */
-public class Persona {
+import java.io.Serializable;
+import java.util.Objects;
+
+//clase javabeans serializable
+
+public class Persona implements Serializable{
+   //atributos privados
+    private String nombre;
+    private String apellido;
+//constructor vacio
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+//get y set
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+//hascode, equals y tostring
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.nombre);
+        hash = 37 * hash + Objects.hashCode(this.apellido);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellido, other.apellido)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+    
     
 }
